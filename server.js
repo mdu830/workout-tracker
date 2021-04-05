0d4ecdf762d1cfa9386dd7523726a9170e430ab7
mdu830 <mdu830@yahoo.com>
2021-04-05 14:52:40 -0400

    added develop folder items & created server.js and html routes

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+// app.use(require("./routes/api.js"));
+app.use(require("./routes/htmlRoutes.js"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
